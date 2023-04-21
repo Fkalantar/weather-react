5885028e16d3ed4ae57344b9ded56ba81c55cc34
Fkalantar <fatanehkalantar@gmail.com>
2023-04-21 06:25:12 -0700

    Changed the loader to orange

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -12,7 +12,7 @@ axios.get(apiUrl).then(handleResponse);
     return (
         <Puff
          type="puff" 
-         color="#00bfff" 
+         color="orange" 
          height={100} 
          width={100} 
          timeout={3000} 
