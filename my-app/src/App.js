0c66ead70042f5f94a83190a6c2d93e778ee779c
Fkalantar <fatanehkalantar@gmail.com>
2023-04-21 06:17:09 -0700

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello from React</h1>
+      <Weather city="Paris" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
